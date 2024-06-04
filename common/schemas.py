6a4509ff9f77c9698af581d2c6ae159a0950555a
p-ferreira <pe_drojunior@hotmail.com>
2024-06-04 21:46:55 +0000

    adds default value to query chat schema

--- common/schemas.py
+++ common/schemas.py
@@ -2,14 +2,14 @@ from marshmallow import Schema, fields
 
 
 class QueryChatSchema(Schema):
-    k = fields.Int(description="The number of miners from which to request responses.")
+    k = fields.Int(description="The number of miners from which to request responses.", default=10)
     exclude = fields.List(fields.Str(), description="A list of roles or agents to exclude from querying.")
     roles = fields.List(fields.Str(), required=True, description="The roles of the agents to query.")
     messages = fields.List(fields.Str(), required=True, description="The messages to be sent to the network.")
-    timeout = fields.Int(description="The time in seconds to wait for a response.")
-    prefer = fields.Str(description="The preferred response format, can be either 'longest' or 'shortest'.")
+    timeout = fields.Int(description="The time in seconds to wait for a response.", default=10)
+    prefer = fields.Str(description="The preferred response format, can be either 'longest' or 'shortest'.", default='longest')
     sampling_mode = fields.Str(
-        description="The mode of sampling to use, defaults to 'random'. Can be either 'random' or 'top_incentive'.")
+        description="The mode of sampling to use, defaults to 'random'. Can be either 'random' or 'top_incentive'.", default='top_incentive')
 
 
 class StreamChunkSchema(Schema):

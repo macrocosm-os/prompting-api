864cff1742d385603c17be8e9bfecc4f9921786d
steffencruz <steffenjcruz@gmail.com>
2024-07-19 14:23:46 +0000

    Fix bugs

--- app.py
+++ app.py
@@ -6,93 +6,99 @@ import plotly.express as px
 import utils
 
 _ = """
-Proteins folded (delta 24hr)
-Current proteins folding (24hr)
-Average time to fold trend
-Refolded proteins (group by run id and pdb id and get unique)
-Simulation duration distribution
+[x] Define KPIs: Number of steps, number of completions and total generated tokens
+[x] Data pipeline I: pull run summary data from wandb
+[x] Data pipeline II: pull run event data from wandb (max 500 steps per run)
+[x] Task trends: Number of tasks over time
+[x] Reward trends I: average reward over time, by task
+[x] Reward trends II: average nonzero reward over time, by task
+[x] Reward trends III: average nonzero normalized reward over time, by task
+[x] Explain trends: show release dates to indicate sudden changes 
+[ ] Miner trends: associate uids with miner rankings and plot top miner rewards vs network avg 
+[ ] Baseline rewards I: compare the network trends with baseline model gpt-3.5-turbo
+[ ] Baseline rewards II: compare the network trends with baseline model gpt-4o
+[ ] Baseline rewards III: compare the network trends with baseline model zephyr
+[ ] Baseline rewards IV: compare the network trends with baseline model solar
+[ ] Baseline rewards V: compare the network trends with baseline model llama3 8B
+[ ] Baseline rewards VI: compare the network trends with baseline model llama3 70B
+
+---------
 """
 
-UPDATE_INTERVAL = 3600
-
-
-st.title('Folding Subnet Dashboard')
+st.title('Prompting Subnet Dashboard')
 st.markdown('<br>', unsafe_allow_html=True)
 
 # reload data periodically
-df = utils.build_data(time.time()//UPDATE_INTERVAL)
-st.toast(f'Loaded {len(df)} runs')
+state_vars = utils.load_state_vars()
 
-# TODO: fix the factor for 24 hours ago
-runs_alive_24h_ago = (df.last_event_at > pd.Timestamp.now() - pd.Timedelta('1d'))
-df_24h = df.loc[runs_alive_24h_ago]
-# correction factor to account for the fact that the data straddles the 24h boundary
-# correction factor is based on the fraction of the run which occurred in the last 24h
-# factor = (df_24h.last_event_at - pd.Timestamp.now() + pd.Timedelta('1d')) / pd.Timedelta('1d')
-
+df_runs = state_vars['df_runs']
+df_runs_24h = state_vars['df_runs_24h']
+df_vali = state_vars['df_vali']
+df_events = state_vars['df_events']
+df_task_counts = state_vars['df_task_counts']
+df_m = state_vars['metagraph']
+st.toast(f'Loaded {len(df_runs)} runs')
 
 #### ------ PRODUCTIVITY ------
 
 # Overview of productivity
 st.subheader('Productivity overview')
-st.info('Productivity metrics show how many proteins have been folded, which is the primary goal of the subnet. Metrics are estimated using weights and biases data combined with heuristics.')
+st.info('Productivity metrics show how much data has been created by subnet 1')
 
-productivity = utils.get_productivity(df)
-productivity_24h = utils.get_productivity(df_24h)
+productivity = utils.get_productivity(df_runs)
+productivity_24h = utils.get_productivity(df_runs_24h)
 
 
-m1, m2, m3 = st.columns(3)
-m1.metric('Unique proteins folded', f'{productivity.get("unique_folded"):,.0f}', delta=f'{productivity_24h.get("unique_folded"):,.0f} (24h)')
-m2.metric('Total proteins folded', f'{productivity.get("total_simulations"):,.0f}', delta=f'{productivity_24h.get("total_simulations"):,.0f} (24h)')
-m3.metric('Total simulation steps', f'{productivity.get("total_md_steps"):,.0f}', delta=f'{productivity_24h.get("total_md_steps"):,.0f} (24h)')
+m1, m2, m3, m4 = st.columns(4)
+m1.metric('Competition duration', f'{productivity.get("duration").days} days')
+m2.metric('Total events', f'{productivity.get("total_events")/1e6:,.2f}M', delta=f'{productivity_24h.get("total_events")/1e6:,.2f}M (24h)')
+m3.metric('Total completions', f'{productivity.get("total_completions")/1e9:,.2f}B', delta=f'{productivity_24h.get("total_completions")/1e9:,.2f}B (24h)')
+m4.metric('Total dataset tokens', f'{productivity.get("total_tokens")/1e9:,.2f}B', delta=f'{productivity_24h.get("total_tokens")/1e9:,.2f}B (24h)')
 
 st.markdown('<br>', unsafe_allow_html=True)
 
-time_binned_data = df.set_index('last_event_at').groupby(pd.Grouper(freq='12h'))
-
-PROD_CHOICES = {
-    'Unique proteins folded': 'unique_pdbs',
-    'Total simulations': 'total_pdbs',
-    'Total simulation steps': 'total_md_steps',
-}
-prod_choice_label = st.radio('Select productivity metric', list(PROD_CHOICES.keys()), index=0, horizontal=True)
-prod_choice = PROD_CHOICES[prod_choice_label]
-steps_running_total = time_binned_data[prod_choice].sum().cumsum()
 st.plotly_chart(
-    # add fillgradient to make it easier to see the trend
-    px.area(steps_running_total, y=prod_choice, 
-            labels={'last_event_at':'', prod_choice: prod_choice_label},
-    ).update_traces(fill='tozeroy'),
+    px.area(df_task_counts, y=df_task_counts.columns, title='Data Created by Task', 
+            labels={'created_at':'','value':'Total data created'},
+            ),
     use_container_width=True,
 )
 
 st.markdown('<br>', unsafe_allow_html=True)
 
-
-#### ------ THROUGHPUT ------
-st.subheader('Throughput overview')
-
-st.info('Throughput metrics show the total amount of data sent and received by the validators. This is a measure of the network activity and the amount of data that is being processed by the subnet.')
-
-MEM_UNIT = 'GB' #st.radio('Select memory unit', ['TB','GB', 'MB'], index=0, horizontal=True)
-
-data_transferred = utils.get_data_transferred(df,unit=MEM_UNIT)
-data_transferred_24h = utils.get_data_transferred(df_24h, unit=MEM_UNIT)
-
-m1, m2, m3 = st.columns(3)
-m1.metric(f'Total sent data ({MEM_UNIT})', f'{data_transferred.get("sent"):,.0f}', delta=f'{data_transferred_24h.get("sent"):,.0f} (24h)')
-m2.metric(f'Total received data ({MEM_UNIT})', f'{data_transferred.get("received"):,.0f}', delta=f'{data_transferred_24h.get("received"):,.0f} (24h)')
-m3.metric(f'Total transferred data ({MEM_UNIT})', f'{data_transferred.get("total"):,.0f}', delta=f'{data_transferred_24h.get("total"):,.0f} (24h)')
+# Overview of productivity
+st.subheader('Improvement overview')
+st.info('Subnet 1 is an endlessly improving system, where miners compete to produce high quality responses to a range of challenging tasks')
 
 
-IO_CHOICES = {'total_data_sent':'Sent', 'total_data_received':'Received'}
-io_running_total = time_binned_data[list(IO_CHOICES.keys())].sum().rename(columns=IO_CHOICES).cumsum().melt(ignore_index=False)
-io_running_total['value'] = io_running_total['value'].apply(utils.convert_unit, args=(utils.BASE_UNITS, MEM_UNIT))
+TASK_CHOICES = {
+    'Question answering': 'qa',
+    'Summarization': 'summarization',
+    'Date-based question answering': 'date_qa',
+    'Math': 'math',
+    'Generic instruction': 'generic',
+    'Sentiment analysis': 'sentiment',
+    'Translation': 'translation',
+}
+
+with st.expander('Advanced settings'):
+    c1, c2 = st.columns(2)
+    remove_zero_rewards = c1.checkbox('Exclude zero rewards', value=True, help='Remove completions which scored zero rewards (failed responses, timeouts etc.)')
+    normalize_rewards = c1.checkbox('Normalize rewards', value=True, help='Scale rewards for each task to a maximium value of 1 (approx)')
+    show_releases = c1.checkbox('Show releases', value=False, help='Add annotations which indicate when major releases may have impacted network performance')
+    moving_avg_window = c2.slider('Moving avg. window', min_value=1, max_value=30, value=14, help='Window size to smooth data and make long term trends clearer')
+
+reward_col = 'normalized_rewards' if normalize_rewards else 'rewards'
+
+df_stats = utils.get_reward_stats(df_events, exclude_multiturn=True, freq='1D', remove_zero_rewards=remove_zero_rewards)
+
+
+task_choice_label = st.radio('Select task', list(TASK_CHOICES.keys()), index=0, horizontal=True)
+task_choice = TASK_CHOICES[task_choice_label]
 
 st.plotly_chart(
-    px.area(io_running_total, y='value', color='variable',
-            labels={'last_event_at':'', 'value': f'Data transferred ({MEM_UNIT})', 'variable':'Direction'},
-    ),
+    # add fillgradient to make it easier to see the trend
+    utils.plot_reward_trends(df_stats, task=task_choice, window=moving_avg_window, col=reward_col, annotate=show_releases, task_label=task_choice_label),
     use_container_width=True,
 )
 
@@ -107,7 +113,6 @@ m1, m2 = st.columns(2)
 ntop = m1.slider('Number of top miners to display', value=10, min_value=3, max_value=50, step=1)
 entity_choice = m2.radio('Select entity', utils.ENTITY_CHOICES, index=0, horizontal=True)
 
-df_m = utils.get_metagraph(time.time()//UPDATE_INTERVAL)
 df_miners = utils.get_leaderboard(df_m, ntop=ntop, entity_choice=entity_choice)
 
 # hide colorbar and don't show y axis
@@ -128,13 +133,13 @@ st.markdown('<br>', unsafe_allow_html=True)
 #### ------ LOGGED RUNS ------
 
 st.subheader('Logged runs')
-st.info('The timeline shows the creation and last event time of each run.')
-st.plotly_chart(
-    px.timeline(df, x_start='created_at', x_end='last_event_at', y='username', color='state',
-                labels={'created_at':'Created at', 'last_event_at':'Last event at', 'username':''},
-                ),
-    use_container_width=True
-)
+# st.info('The timeline shows the creation and last event time of each run.')
+# st.plotly_chart(
+#     px.timeline(df_runs, x_start='created_at', x_end='last_event_at', y='user', color='state',
+#                 labels={'created_at':'Created at', 'last_event_at':'Last event at', 'username':''},
+#                 ),
+#     use_container_width=True
+# )
 
 with st.expander('Show raw run data'):
-    st.dataframe(df)
+    st.dataframe(df_runs)
